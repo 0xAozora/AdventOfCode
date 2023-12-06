316eeca0d3ed3e9cc15bc34889a0233091d9dc2c
0xAozora <atha.ar@arcor.de>
2023-12-07 00:47:59 +0100

    day 4 part 2

--- day4/main.rs
+++ day4/main.rs
@@ -5,12 +5,21 @@ use std::path::Path;
 fn main() {
 
     let mut sum = 0;
+    let mut sum2 = 0;
 
     if let Ok(lines) = read_lines("./input") {
         // Consumes the iterator, returns an (Optional) String
+
+        let mut ring: [i64; 25] = [1;25];
+
+        println!("{:?}", ring);
+
+        let mut r: usize = 0;
+        
         for line in lines {
 
             let mut points = 1;
+            let mut amount = 0;
             if let Ok(l) = line {
                 
                 let numbers: Vec<&str> = l[7..].split('|').collect();
@@ -18,6 +27,7 @@ fn main() {
                 let mut i = 0;
                 while i < numbers[1].len() {
                     if numbers[0].contains(&numbers[1][i..i+3]) {
+                        amount += 1;
                         points = points << 1;
                     }
                     i += 3
@@ -25,12 +35,28 @@ fn main() {
 
             }
 
+            let copies = ring[r];
+            sum2 += copies;
+            ring[r] = 1;
+
+            let mut i = r;
+            while amount > 0 {
+                i += 1;
+                i %= 25;
+                ring[i] += copies;
+                amount -= 1;
+            }
+
+            r += 1;
+            r %= 25;
+
             sum += points >> 1;
         }
     }
 
 
-    print!("{}", sum);
+    println!("{}", sum);
+    println!("{}", sum2);
 
 }
 
