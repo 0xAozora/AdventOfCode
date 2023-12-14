9a84277ad8a27c8ac1074c401cc872097618fdd5
0xAozora <atha.ar@arcor.de>
2023-12-14 01:08:12 +0100

    day 12 part 2

--- day12/main.rs
+++ day12/main.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-use regex::{Regex, Replacer};
+use num_bigint::{BigUint, ToBigUint};
+use num_traits::cast::ToPrimitive;
 
 fn main() {
 
@@ -12,7 +13,6 @@ fn main() {
     if let Ok(lines) = read_lines("./input") {
         // Consumes the iterator, returns an (Optional) String
 
-        let mut ln = 0;
         for line in lines {
             if let Ok(l) = line {
                 
@@ -27,10 +27,9 @@ fn main() {
                     regex_pattern = format!("{}#{{{}}}[^#]*", regex_pattern, n);
                 }
 
-                let pattern = Regex::new(&regex_pattern).unwrap();
-                sum += arrangements(&row, &num, "", &pattern);
+                sum += arrangements(&row, &num);
 
-
+                // Part 2
                 let row5 = format!("{}?{}?{}?{}?{}", row, row, row, row, row);
                 let mut num5: Vec<u8> = vec![0;num.len()*5];
 
@@ -45,108 +44,195 @@ fn main() {
                     i += 1;
                 }
 
-                sum2 += arrangements(&row5, &num5, "", &pattern);
-                println!("{}", ln);
-                ln += 1;
+                sum2 += arrangements(&row5, &num5);
             }
 
         }
     }
-
 
     println!("{}", sum);
     println!("{}", sum2);
 
 }
 
-fn arrangements(row: &str, num: &[u8], prev: &str, pattern: &Regex) -> i32 {
-
-    let mut n = num[0];
+fn arrangements(row: &str, num: &[u8]) -> i128 {
 
     let s: u8 = num[1..].iter().copied().sum();
-    //println!("Sum: {s} Row: {} Len: {}", row.len(), num[1..].len());
-    let d: isize = row.len() as isize - (s as isize + num.len() as isize - 1);
-    if d <= 0 {
+    let end: isize = row.len() as isize - (s as isize + num.len() as isize - 1);
+    if end <= 0 {
         return 0;
     }
 
-    let end = d as usize;
-
-    //println!("End: {end}");
-
-    let mut sum = 0;
+    let mut sum: i128 = 0;
 
     // Skip .
-    let mut i = 0;
-    while i < row.len() {
-        if row.chars().nth(i).unwrap() != '.' {
+    let mut dots = 0;
+    while dots < row.len() {
+        if row.chars().nth(dots).unwrap() != '.' {
             break;
         }
-        i += 1;
+        dots += 1;
     } 
 
-    while i < end {
-
-        let mut c = row.chars().nth(i).unwrap();
-        let first = c== '#';
-
-        let mut j = i;
-        while c == '#' || c == '?' {
-            n -= 1;
-            j += 1;
-            
-            if j >= row.len() {
-                if n == 0 && num.len() == 1 {
-                    let n = num[0] as usize;
-
-                    // let m = prev.to_string() + &row[..i].to_string() + &vec!["#";n].join("") + &row[i+n..].to_string();
-                    // if m == "" {
-                    //     println!("fail")
-                    // }
-                    // println!("{}", m.replace("?", "."));
-                    sum += 1;
-                }
-                return sum;
-            }
-
-            c = row.chars().nth(j).unwrap();
-
-            if n == 0 {
-                if c != '#' {
-                    if num.len() == 1 {
-                        let res = check_rem(&row[j..]);
-                        if res == 1 {
-                            // let n = num[0] as usize;
-                            // let m =  prev.to_string() + &row[..i].to_string() + &vec!["#";n].join("") + &row[i+n..].to_string();
-                            // if m == "" {
-                            //     println!("fail")
-                            // }
-                            sum += 1;
-                           //println!("{}", m.replace("?", "."))
-                        }
-                    } else if j < row.len() - 1 {
-                        let n = num[0] as usize;
-                        //let m = prev.to_string() + &row[..i].to_string() + &vec!["#";n].join("") + &row[i+n..i+n+1].to_string();
-                        let m = "";
-                        sum += arrangements(&row[j+1..], &num[1..], &m, pattern);
-                    }
-                }
-                break;
-            }
+    // Count '?'
+    let mut h: i16 = 0; // #
+    let mut q = 0; // ?
+    let mut i: usize = dots;
+    while (i as usize) < row.len() {
+        match row.chars().nth(i).unwrap() {
+            '#' => {h = 1; break},
+            '.' => break,
+            _ => q += 1
         }
-
-        if first {
-            break;
-        }
-
-        n = num[0];
         i += 1;
     }
 
+    if h == 0 && q != 0 {
+        let mut n = 0;
+        while n < num.len() {
+            let c = questionmark_combinations((i-dots) as i128, &num[0..n+1]);
+            if c == 0 {
+                break;
+            }
+            if n == num.len() - 1 {
+                sum += c * check_rem(&row[i..]);
+                break;
+            }
+
+            if i < row.len() {
+                sum += c * arrangements(&row[i+1..], &num[n+1..]);
+            }
+            
+            n += 1
+        }
+
+        // Try Skipping this block
+        if i < row.len() {
+            sum += arrangements(&row[i+1..], num);
+        }
+        return sum
+    }
+
+    // Count '#'
+    if h == 1 {
+
+        i += 1;
+        while i < row.len() {
+            match row.chars().nth(i).unwrap() {
+                '#' => h += 1,
+                _ => break
+            }
+            i += 1;
+        }
+
+        // Combinations
+        let mut n = 0;
+        while n < num.len() {
+
+            if (num[n] as i16) < h {
+                n += 1;
+                continue;
+            }
+            
+            let mut c: i128 = 1;
+            let shift = num[n] as i16 - h;
+
+            // Shift # Match
+            let mut s = 0;
+            while s <= shift {
+
+
+                // Valid?
+                let offset = i + s as usize;
+                if offset > row.len() || row.chars().nth(offset-1).unwrap() == '.' {
+                    break;
+                }
+
+                if offset < row.len() {
+                    if row.chars().nth(offset).unwrap() == '#' {
+                        s += 1;
+                        continue;
+                    }
+                } else if offset > row.len() {
+                    break;
+                }
+
+                let q_rem = q - (shift-s);
+                if q_rem < 0 {
+                    s += 1;
+                    continue;
+                }
+
+                // Check Prev ? Block Combinations
+                if n > 0 {
+                    c = questionmark_combinations((q_rem - 1) as i128, &num[..n]);
+                    if c == 0 {
+                        s += 1;
+                        continue;
+                    }
+                }
+
+                if n == num.len() - 1 {
+                    sum += c * check_rem(&row[offset..]);
+                    s += 1;
+                    continue;
+                }
+    
+                if offset < row.len() {
+                    sum += c * arrangements(&row[offset+1..], &num[n+1..]);
+                }
+
+
+                s += 1;
+            }
+
+            n += 1;
+        }
+
+    }
+    
     return sum;
 }
 
-fn check_rem(row: &str) -> i32 {
+fn questionmark_combinations(amount: i128, num: &[u8]) -> i128 {
+
+    let mut c: i128;
+    let gaps: i128 = num.len() as i128 - 1;
+    let sum: u8 = num.iter().copied().sum();
+    let mut size = sum as i128 + gaps;
+    
+    if amount < size {
+        return 0;
+    }
+
+    c = (amount - size) + 1;
+    if num.len() == 1 {
+        return c;
+    }
+
+    size += 1;
+    let mut n = 1; 
+    while size <= amount {
+        c += ((amount - size) + 1) * combinations(n, gaps);
+        size += 1;
+        n += 1;
+    }
+
+    return c;
+}
+
+fn factorial(n: i128) -> BigUint {
+    if n == 0 || n == 1 {
+        return 1.to_biguint().unwrap();
+    }
+    n.to_biguint().unwrap() * factorial(n - 1)
+}
+
+fn combinations(n: i128, x: i128) -> i128 {
+    return (factorial(n+x-1) / (factorial(n) * factorial(x-1))).to_i128().unwrap();
+}
+
+fn check_rem(row: &str) -> i128 {
     
     let mut i = 0;
     while i < row.len() {
